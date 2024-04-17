621624fdfe6c2d9070e97adc3574a06823f43004
achyutbenz19 <achyut.benz@gmail.com>
2024-04-16 23:17:59 -0400

    icon

--- thock/App.swift
+++ thock/App.swift
@@ -3,7 +3,7 @@ import SwiftUI
 @main
 struct thockApp: App {
     var body: some Scene {
-        MenuBarExtra("Menu", systemImage: "sparkles") {
+        MenuBarExtra("Menu", systemImage: "keyboard.macwindow") {
             Menu()
         }
         .menuBarExtraStyle(.window)
--- thock/Menu.swift
+++ thock/Menu.swift
@@ -3,9 +3,6 @@ import SwiftUI
 struct Menu: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
         }
         .padding()
