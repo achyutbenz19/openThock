24769ed60c32ee5fc330338b77b35ae234facf0d
achyutbenz19 <achyut.benz@gmail.com>
2024-04-17 14:19:54 -0400

    listener

--- thock/App.swift
+++ thock/App.swift
@@ -3,15 +3,14 @@ import SwiftUI
 @main
 struct ThockApp: App {
     @StateObject var settingsViewModel = SettingsViewModel()
+    @StateObject private var keyListener = KeyListener()
     
     var body: some Scene {
+        
         MenuBarExtra("Menu", systemImage: "keyboard.macwindow") {
             Menu(settingsViewModel: settingsViewModel)
         }
         .menuBarExtraStyle(.window)
-        
-        WindowGroup {
-            Sound(settingsViewModel: settingsViewModel)
-        }
+        .environmentObject(keyListener)
     }
 }
---
+++ thock/Listener.swift
@@ -0,0 +1,46 @@
+import Cocoa
+import SwiftUI
+
+class KeyListener: ObservableObject {
+    private var eventTap: CFMachPort?
+
+    init() {
+        setupEventTap()
+    }
+
+    deinit {
+        stopListening()
+    }
+
+    private func setupEventTap() {
+        let eventMask = (1 << CGEventType.keyDown.rawValue) 
+        eventTap = CGEvent.tapCreate(
+            tap: .cgSessionEventTap,
+            place: .headInsertEventTap,
+            options: .defaultTap,
+            eventsOfInterest: CGEventMask(eventMask),
+            callback: { (proxy, type, event, refcon) -> Unmanaged<CGEvent>? in
+                if type == .keyDown {
+                    let keyCode = event.getIntegerValueField(.keyboardEventKeycode)
+                    print("Key Pressed: \(keyCode)")
+                }
+
+                return Unmanaged.passRetained(event)
+            },
+            userInfo: nil
+        )
+
+        if let eventTap = eventTap {
+            let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
+            CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+            CGEvent.tapEnable(tap: eventTap, enable: true)
+        }
+    }
+
+
+    func stopListening() {
+        if let eventTap = eventTap {
+            CGEvent.tapEnable(tap: eventTap, enable: false)
+        }
+    }
+}
