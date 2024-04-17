b492fca59a3260e6d0054758b719f8a2e46fe8ac
achyutbenz19 <achyut.benz@gmail.com>
2024-04-16 22:09:49 -0400

    init

---
+++ thock/thockApp.swift
@@ -0,0 +1,17 @@
+//
+//  thockApp.swift
+//  thock
+//
+//  Created by Achyut Krishna Byanjankar on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct thockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
