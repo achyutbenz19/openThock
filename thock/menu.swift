cbff892dc5cded7966a580b5224f423731e2da31
achyutbenz19 <achyut.benz@gmail.com>
2024-04-16 22:32:23 -0400

    added menu

--- thock/menu.swift
+++ thock/menu.swift
@@ -1,13 +1,6 @@
-//
-//  ContentView.swift
-//  thock
-//
-//  Created by Achyut Krishna Byanjankar on 4/16/24.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+struct Menu: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +13,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    Menu()
 }
--- thock/thockApp.swift
+++ thock/thockApp.swift
@@ -1,17 +1,11 @@
-//
-//  thockApp.swift
-//  thock
-//
-//  Created by Achyut Krishna Byanjankar on 4/16/24.
-//
-
 import SwiftUI
 
 @main
 struct thockApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("Menu", systemImage: "sparkles") {
+            Menu()
         }
+        .menuBarExtraStyle(.window)
     }
 }
