d314a3ced9a1fb19a008cff59907c1ff97ba04a4
achyutbenz19 <achyut.benz@gmail.com>
2024-04-17 20:30:52 -0400

    bg color

--- thock/Menu.swift
+++ thock/Menu.swift
@@ -42,6 +42,7 @@ struct Menu: View {
         }
         .padding(8)
         .frame(width: 200)
+        .background(Color(red: 0.95, green: 0.95, blue: 0.95))
     }
 
     private func quitApplication() {
